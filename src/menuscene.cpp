c31c69ae83752ef3b7ca64d4896146fe00cfeb0f
Eugene Gritz <spicywin@gdlbo.net>
2023-03-10 03:15:58 +0600

    Adjust vertex shader for borders

--- src/menuscene.cpp
+++ src/menuscene.cpp
@@ -17,10 +17,12 @@ MenuScene::MenuScene(sf::RenderTarget *target) : Scene(target) {
     "void main() {" \
     "    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;" \
     "    gl_TexCoord[0] = gl_TextureMatrix[0] * gl_MultiTexCoord0;" \
-    "    gl_FrontColor = vec4(cos(gl_Position.x+1.0+off)," \
-    "                         cos(gl_Position.x+off)," \
-    "                         cos(gl_Position.x-1.0+off)," \
-    "                         1.0);" \
+    "    gl_FrontColor = vec4(" \
+    "        (cos(2.0 * gl_Position.x - 0.5 + 1.5 + off) + 1.0) / 2.5f," \
+    "        (cos(2.0 * gl_Position.x - 0.5       + off) + 1.0) / 2.5f," \
+    "        (cos(2.0 * gl_Position.x - 0.5 - 1.0 + off) + 1.0) / 2.5f," \
+    "        1.0" \
+    "    );" \
     "}";
     _borderShader.loadFromMemory(vertexShader, sf::Shader::Vertex);
     _borderShader.setUniform("off", _colorOffset);
